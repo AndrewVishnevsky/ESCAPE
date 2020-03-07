8e84015b7393570a79c7817a68c5261fa3d85cc7
Andrew Vishnevsky <vishnev93@gmail.com>
2020-03-07 20:32:19 +0900

    UE_LOG and pointers practice
    I refresh my memory after a long lack of practice

---
+++ Source/ESCAPE/Private/WorldPos.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPos.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPos::UWorldPos()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPos::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//FString Log = TEXT("Hello everyone to my FIRST GAME!!!");
+	//FString* PrtLog = &Log;
+
+	//Log.Len();
+
+	//(*PrtLog).Len();
+	//PrtLog->Len();
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), **PrtLog);
+
+	//UE_LOG(LogTemp, Warning, TEXT("This is a Yellow Warning test message!"));
+	//UE_LOG(LogTemp, Error, TEXT("This is a Red Error test message!"));
+	//UE_LOG(LogTemp, Log, TEXT("This is a Grey log test message!"));
+	FString ObjName{};
+	ObjName=GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *ObjName);
+	
+	
+}
+
+
+// Called every frame
+void UWorldPos::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
